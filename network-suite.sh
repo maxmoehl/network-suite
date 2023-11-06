d91c7907626a2e0580f92a06495a927b764042fb
Maximilian Moehl <maximilian@moehl.eu>
2023-11-06 15:32:28 +0100

    refactor: overhaul code

--- network-suite.sh
+++ network-suite.sh
@@ -11,9 +11,19 @@ __error() {
 # don't want that.
 alias __shift='test "${#}" -gt 0 && shift'
 
+# Usage: __validate NAME
+__validate() {
+	# reserved names
+	if [ "${1}" = "lo" ]; then __error "'lo' is a reserved name"; fi
+	if [ "${1}" = "br0" ]; then __error "'br0' is a reserved name"; fi
+
+	# must be a valid device name
+	if ! expr "${1}" : "^[a-zA-Z0-9_-]\{1,15\}$" > /dev/null; then __error "'${1}' is not a valid name"; fi
+}
+
 # Execute ip commands or dump them to stdout.
 __ip() {
-	if test "${Dump}" -eq 1; then
+	if [ "${Dump}" -eq 1 ]; then
 		echo "ip ${*}"
 	else
 		ip "${@}"
@@ -22,9 +32,10 @@ __ip() {
 
 # Usage: __host_add NAME
 __host_add() {
-	_host_name="host-${1:?error: host name is empty}"
-	__ip netns add "${_host_name}"
-	__ip -n "${_host_name}" link set lo up
+	_host_name="${1:?error: NAME is empty}"; __validate "${_host_name}"
+
+	__ip netns add "host-${_host_name}"
+	__ip -n "host-${_host_name}" link set lo up
 }
 
 __host_show() {
@@ -33,35 +44,39 @@ __host_show() {
 
 # Usage: __host_del NAME
 __host_del() {
-	_host_name="host-${1:?error: host name is empty}"
-	__ip netns delete "${_host_name}"
+	_host_name="host-${1:?error: host name is empty}"; __validate "${_host_name}"
+
+	__ip netns delete "host-${_host_name}"
 }
 
-# Usage: __host_connect NAME NETWORK DEVICE NETWORK-DEVICE IP
+# Usage: __host_connect NAME NETWORK IP
 __host_connect() {
-	_host_name="host-${1:?error: host name is empty}"
-	_host_connect_net="network-${2:?error: network is empty}"
-	_host_connect_dev="${3:?error: device name is empty}"
-	_host_connect_net_dev="${4:?error: network device name is empty}"
-	_host_connect_ip="${5:?error: ip is empty}"
+	_host_name="${1:?error: NAME name is empty}"; __validate "${_host_name}"
+	_host_connect_net="${2:?error: NETWORK is empty}"; __validate "${_host_connect_net}"
+	_host_connect_ip="${3:?error: IP is empty}"
+	_host_connect_host_dev="${_host_connect_net}"
+	_host_connect_net_dev="${_host_name}"
 
-	__ip -n "${_host_connect_net}" link show br0 > /dev/null || __error "unable to verify that br0 exists in ${_host_connect_net}"
+	# only run this check if we are actually executing
+	if [ "${Dump}" -eq 0 ]; then
+		ip -n "network-${_host_connect_net}" link show br0 > /dev/null || __error "unable to verify that br0 exists in ${_host_connect_net}"
+	fi
 
-	__ip -n "${_host_connect_net}" link add "${_host_connect_net_dev}" type veth peer name "${_host_connect_dev}" netns "${_host_name}"
-	__ip -n "${_host_connect_net}" link set "${_host_connect_net_dev}" master br0
+	__ip -n "network-${_host_connect_net}" link add "${_host_connect_net_dev}" type veth peer name "${_host_connect_host_dev}" netns "host-${_host_name}"
+	__ip -n "network-${_host_connect_net}" link set "${_host_connect_net_dev}" master br0
 
-	__ip -n "${_host_connect_net}" addr add "${_host_connect_ip}" dev "${_host_connect_net_dev}"
-	__ip -n "${_host_name}" addr add "${_host_connect_ip}" dev "${_host_connect_dev}"
+	__ip -n "network-${_host_connect_net}" addr add "${_host_connect_ip}" dev "${_host_connect_net_dev}"
+	__ip -n "host-${_host_name}" addr add "${_host_connect_ip}" dev "${_host_connect_host_dev}"
 
-	__ip -n "${_host_connect_net}" link set "${_host_connect_net_dev}" up
-	__ip -n "${_host_name}" link set "${_host_connect_dev}" up
+	__ip -n "network-${_host_connect_net}" link set "${_host_connect_net_dev}" up
+	__ip -n "host-${_host_name}" link set "${_host_connect_host_dev}" up
 }
 
 # Usage: __host_shell NAME
 __host_shell() {
-	_host_name="host-${1:?error: host name is empty}"
+	_host_name="${1:?error: NAME is empty}"; __validate "${_host_name}"
 
-	if test -z "${SHELL}"; then
+	if [ -z "${SHELL}" ]; then
 		# shellcheck disable=SC2016
 		__error '$SHELL is empty'
 	fi
@@ -69,15 +84,17 @@ __host_shell() {
 	# we are probably run as sudo, try to guess the real user
 	_host_exec_uid="${SUDO_UID:-0}"
 
-	__ip netns exec "${_host_name}" su "$(id -un "${_host_exec_uid}")" --login
+	__ip netns exec "host-${_host_name}" su "$(id -un "${_host_exec_uid}")" --login
 }
 
 # Usage: __host_ip NAME IP_COMMAND...
 __host_ip() {
-	_host_name="host-${1:?error: host name is empty}"
+	_host_name="${1:?error: NAME is empty}"; __validate "${_host_name}"
 	__shift
 
-	__ip -n "${_host_name}" "${@}"
+	if [ "${#}" -eq 0 ]; then __error "IP_COMMAND is empty"; fi
+
+	__ip -n "host-${_host_name}" "${@}"
 }
 
 __host() {
@@ -97,13 +114,14 @@ __host() {
 
 # Usage: __net_add NAME
 __net_add() {
-	_net_name="network-${1:?error: network name is empty}"
-	__ip netns add "${_net_name}"
-	__ip -n "${_net_name}" link set lo up
+	_net_name="${1:?error: NAME is empty}"; __validate "${_net_name}"
+
+	__ip netns add "network-${_net_name}"
+	__ip -n "network-${_net_name}" link set lo up
 
 	# peers on a network are connected using a bridge
-	__ip -n "${_net_name}" link add br0 type bridge
-	__ip -n "${_net_name}" link set br0 up
+	__ip -n "network-${_net_name}" link add br0 type bridge
+	__ip -n "network-${_net_name}" link set br0 up
 }
 
 __net_show() {
@@ -112,15 +130,19 @@ __net_show() {
 
 # Usage: __net_del NAME
 __net_del() {
-	_net_name="network-${1:?error: network name is empty}"
-	__ip netns delete "${_net_name}"
+	_net_name="${1:?error: NAME is empty}"; __validate "${_net_name}"
+
+	__ip netns delete "network-${_net_name}"
 }
 
-# Usage: __net_ip NAME IP_COMMAND
+# Usage: __net_ip NAME IP_COMMAND...
 __net_ip() {
-	_net_name="network-${1:?error: network name is empty}"
+	_net_name="${1:?error: NAME is empty}"; __validate "${_net_name}"
 	__shift
-	__ip -n "${_net_name}" "${@}"
+
+	if [ "${#}" -eq 0 ]; then __error "IP_COMMAND is empty"; fi
+
+	__ip -n "network-${_net_name}" "${@}"
 }
 
 __net() {
@@ -154,7 +176,7 @@ __help() {
 			echo "    add     NAME"
 			echo "    show"
 			echo "    delete  NAME"
-			echo "    connect NAME NETWORK DEVICE NETWORK-DEVICE IP"
+			echo "    connect NAME NETWORK IP"
 			echo "    ip      NAME IP_COMMAND"
 			echo "  help"
 			echo "    net"
@@ -200,10 +222,9 @@ __help() {
 			echo "ns host { add | del } NAME"
 			echo "  Create or delete the named host."
 			echo
-			echo "ns host connect NAME NETWORK DEVICE NETWORK-DEVICE IP"
-			echo "  Add the host with NAME to NETWORK. Each side will get an device. DEVICE is the"
-			echo "  name of the interface at the host and NETWORK-DEVICE will used in the network."
-			echo "  IP will be assigned to both interfaces."
+			echo "ns host connect NAME NETWORK IP"
+			echo "  Add the host with NAME to NETWORK. Each side will get an device. IP will be"
+			echo "  assigned to both interfaces."
 			echo
 			echo "ns host shell NAME"
 			echo "  Spawn a shell on the host with NAME. The command will try to identify the"
